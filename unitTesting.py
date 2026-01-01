eb2d2601ed6ae44e35be6e89f235e345db191583
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2026-01-01 01:48:38 +0000

    modification: added tests to see if columns in df are correct

--- unitTesting.py
+++ unitTesting.py
@@ -14,13 +14,27 @@ class my_unit_tests(unittest.TestCase):
 
     # === Tests if the csvs file has been saved/exist ===
     def test_csv_file_exists(self):
-
-        self.assertTrue(os.path.isfile(poverty_csv_country))
-        self.assertTrue(os.path.isfile(poverty_csv_world))
+        self.assertTrue(os.path.isfile(poverty_csv_region))
         self.assertTrue(os.path.isfile(education_csv))
         self.assertTrue(os.path.isfile(mmr_csv))
         self.assertTrue(os.path.isfile(income_classification_csv))
-      
+    
+    # === Tests if the poverty_df has the right columns
+    def test_df_poverty_headings(self):
+        self.assertEqual(list(poverty_df), ['Country', 'Year', 'PR'])
+
+    # === Tests if the education_df has the right columns
+    def test_df_education_headings(self):
+        self.assertEqual(list(education_df), ['Country', 'Year', 'PCR'])
+
+    # === Tests if the MMR_df has the right columns
+    def test_df_MMR_headings(self):
+        self.assertEqual(list(MMR_df), ['Year', 'Country', 'MMR'])
+
+    # === Tests if the MMR_df_income has the right columns
+    def test_df_MMR_income_headings(self):
+        self.assertEqual(list(MMR_df_income), ['Year', 'Income group', 'Mean_MMR'])
+
 
     # === Tests if read_functions returns dataframes === 
     def test_df_returned(self):
@@ -38,6 +52,13 @@ class my_unit_tests(unittest.TestCase):
         slope , correlation_coefficient , r_squared_value = plot_scatter_poverty_MMR()
         self.assertTrue(-1 <= correlation_coefficient <= 1)
         self.assertTrue(-1 <= r_squared_value <= 1)
+
+    # === Checks if function returns a correlation and r squared value between -1 to 1 ===
+    def test_df_world_only(self):
+        poverty_df_world, education_df_world, MMR_df_world = world_filters()
+        self.assertTrue((poverty_df_world["Country"] == "World").all())
+        self.assertTrue((education_df_world["Country"] == "World").all())
+        self.assertTrue((MMR_df_world["Country"] == "World").all())
 
     # === Checks if function returns a correlation and r squared value between -1 to 1 ===
     def test_df_world_only(self):
